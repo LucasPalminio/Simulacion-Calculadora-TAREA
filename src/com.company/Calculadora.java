1b1ba433ab5b99e9e44f1e4c7eeff3808be66250
MrLuck-7 <lpalminio@gmail.com>
2020-04-30 12:57:18 -0400

    Avanzamos en el menu y agregamos los metodos de suma y resta

--- src/com.company/Calculadora.java
+++ src/com.company/Calculadora.java
@@ -6,31 +6,74 @@ public class Calculadora {
     public static Scanner teclado = new Scanner(System.in);
     private int num1;
     private int num2;
+    private int resultado;
 
     public void menu() {
         System.out.println("=========================================");
         System.out.println("==============|Calculadora|==============");
         System.out.println("=========================================");
+
+
+        String[] simbolos = {"+", "-", "*", "/", " elevado a ", "¿Cual de los dos numeros es mayor? ", "Modificar Los Numeros", "Salir"};
+        System.out.println(">>>>>>Seleccione la opcion deseada<<<<<<");
+        for (int i = 0; i < simbolos.length; i++) {
+            System.out.print("(" + (i + 1) + ") ");
+            if (i <= 4) {
+                System.out.println(num1 + simbolos[i] + num2 + " = ?");
+            } else {
+                System.out.println(simbolos[i]);
+            }
+
+
+        }
+        int opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1: //sumar
+                resultado = suma();
+                System.out.println(num1 + "+" + num2 + "=" + resultado);
+                break;
+            case 2:
+                resultado = restar();
+                System.out.println(num1 + "-" + num2 + "=" + resultado);
+                break;
+            case 7:
+                ingresarLosNumeros();
+                break;
+            case 8:
+                System.out.println("Hasta la proxima!");
+                return;
+            default:
+                System.out.println("Error, la opcion ingresada es incorrecta, intentelo Nuevamete");
+                menu();
+
+        }
+        menu();
+
+    }
+
+    private int suma() {
+        return num1 + num2;
+    }
+
+    private int restar() {
+        return num1 - num2;
+    }
+
+    public void ingresarLosNumeros() {
         num1 = ingresarUnNumero("Ingrese el primer Numero");
         num2 = ingresarUnNumero("Ingrese el segundo Numero");
-
-        System.out.println("\nLos numeros que usted ingreso son el "+num1+" y "+num2);
-        String[] opciones = {"Sumar dos numeros", "Restar dos numeros","Multiplicar dos numeros","Dividir dos números","Potencia de un numero","Salir"};
-        System.out.println(">>>>>>Seleccione la opcion deseada<<<<<<");
-        for (int i = 0; i < opciones.length ; i++) {
-            System.out.println("("+i+")"+opciones[i]);
-        }
     }
+
     private int ingresarUnNumero(String mensaje) {
 
         while (true) {
             try {
 
-                System.out.print(mensaje+": ");
-                int numero = teclado.nextInt();
-                return numero;
+                System.out.print(mensaje + ": ");
+                return teclado.nextInt();
             } catch (Exception e) {
                 System.out.println("Ocurrio un error: " + e.getMessage() + ", porfavor intentelo nuevamente");
+                teclado.nextLine();
             }
 
         }
--- src/com.company/Main.java
+++ src/com.company/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
+        calc.ingresarLosNumeros();
         calc.menu();
 
     }
