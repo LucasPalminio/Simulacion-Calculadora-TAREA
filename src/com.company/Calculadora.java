ad4775505e07fc49b101db5627fb0b282884b9de
MrLuck-7 <lpalminio@gmail.com>
2020-04-30 11:48:19 -0400

    -Se agrego atributos a la clase Calculadora.java (num1 y num2) -Se modifico el metodo menu de la clase Calculadora.java (Primero el usuario debe ingresar dos numeros y  luego aparece el menu de opciones) Nota: Faltan los metodos de cada operacion, preguntar al usuario que opcion a elegir

--- src/com.company/Calculadora.java
+++ src/com.company/Calculadora.java
@@ -1,17 +1,40 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Calculadora {
-    public void menu(){
+    public static Scanner teclado = new Scanner(System.in);
+    private int num1;
+    private int num2;
+
+    public void menu() {
         System.out.println("=========================================");
         System.out.println("==============|Calculadora|==============");
         System.out.println("=========================================");
+        num1 = ingresarUnNumero("Ingrese el primer Numero");
+        num2 = ingresarUnNumero("Ingrese el segundo Numero");
+
+        System.out.println("\nLos numeros que usted ingreso son el "+num1+" y "+num2);
+        String[] opciones = {"Sumar dos numeros", "Restar dos numeros","Multiplicar dos numeros","Dividir dos números","Potencia de un numero","Salir"};
         System.out.println(">>>>>>Seleccione la opcion deseada<<<<<<");
-        System.out.println("(1) Sumar dos numeros");
-        System.out.println("(2) Restar dos numeros");
-        System.out.println("(3) Multiplicar dos numeros");
-        System.out.println("(4) Dividir dos números");
-        System.out.println("(5) Calcular  el mayor entre dos numeros");
-        System.out.println("(6) Potencia de un numero");
-        System.out.println("(7) Salir");
+        for (int i = 0; i < opciones.length ; i++) {
+            System.out.println("("+i+")"+opciones[i]);
+        }
+    }
+    private int ingresarUnNumero(String mensaje) {
+
+        while (true) {
+            try {
+
+                System.out.print(mensaje+": ");
+                int numero = teclado.nextInt();
+                return numero;
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage() + ", porfavor intentelo nuevamente");
+            }
+
+        }
+
+
     }
 }
