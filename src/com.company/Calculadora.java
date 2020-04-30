b4f93a73685f770f4bbbc937512cc9c9ceaf1e07
LORENZO <l.devia02@ufromail.cl>
2020-04-30 00:57:06 -0400

    Esquema de las opciones

---
+++ src/com.company/Calculadora.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Calculadora {
+    public void menu(){
+        System.out.println("=========================================");
+        System.out.println("==============|Calculadora|==============");
+        System.out.println("=========================================");
+        System.out.println(">>>>>>Seleccione la opcion deseada<<<<<<");
+        System.out.println("(1) Sumar dos numeros");
+        System.out.println("(2) Restar dos numeros");
+        System.out.println("(3) Multiplicar dos numeros");
+        System.out.println("(4) Dividir dos números");
+        System.out.println("(5) Calcular  el mayor entre dos numeros");
+        System.out.println("(6) Potencia de un numero");
+        System.out.println("(7) Salir");
+    }
+}
